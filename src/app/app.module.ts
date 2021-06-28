2a2c444f1695eae5296b7699eff4c084caf09954
Jorge Cervantes <jorgecdev@gmail.com>
2021-06-27 19:51:52 -0500

    dynamic form, work experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { DynamicFormComponent } from './pages/dynamic-form/dynamic-form.componen
 
 @NgModule({
   declarations: [AppComponent, DynamicFormComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
